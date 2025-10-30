64ef89b4888e131686cbdcba67aa4480b03af878
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-30 14:03:00 +0200

    functionality added

--- src/main/java/controller/BMIController.java
+++ src/main/java/controller/BMIController.java
@@ -1,4 +1,76 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class BMIController {
+    private final String[] languages = {"EN", "FR", "UR", "VI"};
+    private String lang = languages[0];
+
+    @FXML
+    TextField weightField;
+    @FXML
+    TextField heightField;
+
+    @FXML
+    Label weightLabel;
+    @FXML
+    Label heightLabel;
+    @FXML
+    Label bmiLabel;
+    @FXML
+    Label errorMessage;
+
+    @FXML
+    Button calculateButton;
+    @FXML
+    ChoiceBox<String> languageSelect;
+
+    double weight;
+    double height;
+    double result;
+
+    public void initialize() {
+        errorMessage.setText("");
+        createLanguageSelect();
+        calculateButton.setOnAction(event -> {
+            validateValues();
+        });
+    }
+
+    public double calculateBMI() {
+        double meters = height / 100.0;
+        return weight / (meters * meters);
+    }
+
+    public void validateValues() {
+        try {
+            weight = Double.parseDouble(weightField.getText());
+            height = Double.parseDouble(heightField.getText());
+            result = calculateBMI();
+            displayResult();
+            errorMessage.setText("");
+        }
+        catch (Exception e) {
+            errorMessage.setText("invalid_weight_or_height");
+        }
+    }
+
+    public void displayResult() {
+        String text = "bmi_text: ";
+        bmiLabel.setText(text + String.format("%.2f", result));
+    }
+
+    public void createLanguageSelect() {
+        languageSelect.getItems().clear();
+        languageSelect.getItems().addAll(languages);
+        languageSelect.getSelectionModel().selectFirst();
+    }
+
+    public void setLanguage(String language) {
+
+    }
 }
